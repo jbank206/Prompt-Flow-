e5f8e0f1a5535ef5336c1cfa9c3b4f517c4f2070
jbank206 <jcpimpin206@aol.com>
2025-09-07 19:52:24 -0500

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const Stripe = require('stripe');
+const cors = require('cors');
+
+const app = express();
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { amount } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+    });
+    res.send({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+app.listen(3001, () => console.log('Backend running on port 3001'));
